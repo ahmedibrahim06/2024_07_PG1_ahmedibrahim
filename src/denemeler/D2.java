1ad9a3ff58554d4eda26c615bee892705ef88f75
ahmedibrahim06 <onbasiahmet860@gmail.com>
2024-05-06 13:58:56 +0300

    Initial commit

--- src/denemeler/D2.java
+++ src/denemeler/D2.java
@@ -4,7 +4,7 @@ public class D2 {
     public static void main(String[] args) {
         // Değişken tanımlaması:
         int no1 = 58;;
-///besma oyun oynadı.
+///hayirli olsun
         //ön arttırma ve azaltma operatörü:
         System.out.println("(A)"+ no1 + "(++A)" +(++no1));
     }
