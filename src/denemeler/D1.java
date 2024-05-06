a5f46119f5d91307f632b232cd12c0f835887d79
ahmedibrahim06 <onbasiahmet860@gmail.com>
2024-05-06 14:00:50 +0300

    Initial commit

--- src/denemeler/D1.java
+++ src/denemeler/D1.java
@@ -7,18 +7,18 @@ public class D1 {
         String hos = "Kod sayfamıza Hoşgeldiniz:";
         String takma= " SlimStyle" ;
         // String'lerde ekleme operatörü:
-        System.out.println("Merhabalar "+hos+takma);
+        System.out.println("Merhabalar " + hos + takma);
 
         //ekleme çıkarma öperatörü:
-        System.out.println("A + B = " +(no1 + no2));
-        System.out.println("A - B = " +(no1 - no2));
+        System.out.println("A + B = " + (no1 + no2));
+        System.out.println("A - B = " + (no1 - no2));
 
         //çarpma bölme operatörü
-        System.out.println("A * B = " +(no1 * no2));
-        System.out.println("A / B = " +(no1 / 2));
-        System.out.println("A / 3 = " +(no1 / 13));
+        System.out.println("A * B = " + (no1 * no2));
+        System.out.println("A / B = " + (no1 / 2));
+        System.out.println("A / 3 = " + (no1 / 13));
 
         //Mod alma operatörü
-        System.out.println("C % B = " +(no3 % no2));
+        System.out.println("C % B = " + (no3 % no2));
     }
 }
