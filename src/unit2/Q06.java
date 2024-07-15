33c82d1343c7019146dd632da0f82e9369c31200
ahmedibrahim06 <onbasiahmet860@gmail.com>
2024-07-15 11:29:18 +0300

    Initial commit

---
+++ src/unit2/Q06.java
@@ -0,0 +1,16 @@
+package unit2;
+/* (Tam sayının basamak değerlerinin toplamı) 0 ve 1000 aralığında bir tam sayı alan ve
+ basamak değerlerinin toplamını gösteren bir program yazınız.
+  Örneğin; Girilen tam sayı  932, ise basamak değerli toplamı 14’tür.
+  İpucu:  % operatörünü kullanarak basamak değerlerini elde edebilir,
+ / operatörü ile değeri elde edilen basamağı kaldırabilirsiniz. Örneğin, 932 % 10 = 2 ve 932 / 10 = 93.
+ */
+import java.util.Scanner;
+
+public class Q06 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+    }
+}
